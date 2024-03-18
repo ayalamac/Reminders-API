75159974c65f59ba48ec30b047757d564c66ff65
Andrés Ayala <aayalac@glingeriesas.com>
2024-03-17 19:48:52 -0500

    Activar Swagger en todos los ambientes

--- src/CleanArchitecture.Api/Program.cs
+++ src/CleanArchitecture.Api/Program.cs
@@ -15,11 +15,8 @@ var app = builder.Build();
     app.UseExceptionHandler();
     app.UseInfrastructure();
 
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
     app.UseHttpsRedirection();
     app.UseAuthorization();
